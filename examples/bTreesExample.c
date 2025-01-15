5a8212030e7a1baf1a8902736ac3676616c2b4ba
Valkorz <pivarcivittorio@gmail.com>
2025-01-15 10:44:26 -0300

    Added binary trees example code

---
+++ examples/bTreesExample.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "include/hashMap.h"
+#include "include/calclib.h"
+#include "include/typematch.h"
+#include "include/bintree.h"
+#include "include/csv.h"
+
+int main()
+{
+
+    // Create tree
+    BinaryTree *tree;
+    newTree(&tree, 10);
+
+    printf("\n Number of nodes in the tree: %d", tree->count);
+
+    // Add some nodes...
+    addNode(tree, 5, -1, DEPTH_FIRST_PREORDER);
+    addNode(tree, 10, -1, DEPTH_FIRST_PREORDER);
+    addNode(tree, 12, -1, DEPTH_FIRST_PREORDER);
+    addNode(tree, 35, -1, DEPTH_FIRST_PREORDER);
+
+    system("pause");
+
+    printf("\n Getting value of each node...\n");
+    printf("\n Number of nodes in the tree: %d", tree->count);
+
+    printf("\n Value of node 0: %ld", getNode(tree, 1, DEPTH_FIRST_PREORDER)->id);
+    printf("\n Value of node 1: %ld", getNode(tree, 2, DEPTH_FIRST_PREORDER)->id);
+    printf("\n Value of node 2: %ld", getNode(tree, 3, DEPTH_FIRST_PREORDER)->id);
+    printf("\n Value of node 3: %ld", getNode(tree, 4, DEPTH_FIRST_PREORDER)->id);
+
+    system("pause");
+
+    printf("Node structure: \n ");
+    printTree(tree->root, 0);
+
+    system("pause");
+
+    return 0;
+}
