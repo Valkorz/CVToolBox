0ef4bf96db7315b12ec62d83515a5bc031bd4a29
Valkorz <pivarcivittorio@gmail.com>
2025-02-03 17:43:55 -0300

    removed some old files.

--- examples/queueExample.c
+++ examples/queueExample.c
@@ -2,36 +2,44 @@
 #include <stdlib.h>
 #include "queue.h"
 
-int main(){
-    Queue* queue = newQueue();
-    enqueue(queue, "bananas");
-    enqueue(queue, "apples");
-    enqueue(queue, "oranges");
-    enqueue(queue, "tomatos");
-    printQueue(queue);
+/*
+    This is an example on how to use the queue.h to create a FIFO (First In First Out) structure;
+*/
 
+int main(){
+    printf("-- QUEUE EXAMPLE -- \n \n");
+    Queue* queue = newQueue();
+    char in[100];
+    printf("\n Created new queue...");
+    system("pause");
+    
+    printf("\n Enqueue input, send '#' to STOP...");
+
+    while(1){
+        scanf("%s", in);
+        if(*in == '#') break;
+
+        enqueue(queue, in);
+    }
+
+    printf("\n Finished enqueue process...");
     system("pause");
     system("cls");
 
-    printf("\n Dequeueing...");
-    printf("\n result: %s", dequeue(queue));
-    printf("\n new Queue: ");
-    printQueue(queue);
-    printf("\n result: %s", dequeue(queue));
-    printf("\n new Queue: ");
-    printQueue(queue);
-    printf("\n result: %s", dequeue(queue));
-    printf("\n new Queue: ");
-    printQueue(queue);
-    printf("\n result: %s", dequeue(queue));
-    printf("\n new Queue: ");
-    printQueue(queue);
-    printf("\n result: %s", dequeue(queue));
-    printf("\n new Queue: ");
+    printf("\n Result: ");
     printQueue(queue);
 
+    system("pause");    
+    system("cls");
+
+    printf("\n First element: %s", dequeue(queue));
+    printf("\n Second element: %s", dequeue(queue));
+
+    printf("\n Proceed to free queue...");
     system("pause");
-    free(queue);
+    system("cls");
+
+    freeQueue(queue);
 
     return EXIT_SUCCESS;
 }
